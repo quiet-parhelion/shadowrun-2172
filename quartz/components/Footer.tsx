a53579bb983af51e47e5465845fad57fc05e1987
Quiet Parhelion <quiet.parhelion@gmail.com>
2024-03-02 06:48:11 +0000

    Quartz sync: Mar 2, 2024, 6:48 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,17 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+        <p><a href="https://www.youtube.com/watch?v=Hmd7mtlmYHE">Quiet</a> ⑨ 2024</p>
       </footer>
     )
   }
